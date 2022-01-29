110f366c64291ea83a1e7694b2a6982b516bf84b
goburhan <goburhann@gmail.com>
2022-01-29 16:08:25 +0300

    e

--- src/Components/SocialMedia.js
+++ src/Components/SocialMedia.js
@@ -6,12 +6,12 @@ export default function SocialMedia() {
                 Social Media
                 
                 </div>
-                <div className="grid  sm:grid-cols-5 lg:grid-cols-11  items-center place-items-center mt-12 mb-24 ">
+                <div className="grid sm:gap-16 sm:mr-6 sm:grid-cols-5 lg:grid-cols-11  items-center place-items-center mt-12 mb-24 ">
                 <div className="lg:block sm:hidden"></div>
                 <div className="lg:block sm:hidden"></div>
                 <div className="lg:block sm:hidden"></div>
 
-                <img className="" src="/images/instagram.png" alt="welcome" style={{maxWidth:70}} />
+                <img className="" src="/images/instagram.png" alt="welcome" style={{maxWidth:70 }} />
                 <img className="" src="/images/facebook.png" alt="welcome" style={{maxWidth:70}}/>
                 <img className="" src="/images/twitter.png" alt="welcome" style={{maxWidth:70}}/>
                 <img className="" src="/images/youtube.png" alt="welcome" style={{maxWidth:70}}/>
